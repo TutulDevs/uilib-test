062c8e3201880df0e0a3551c1a422273fb59d500
Tutul <tutulnahid@gmail.com>
2021-08-12 01:21:46 +0600

    userList: search input is there now

--- src/components/listPage/UserList.js
+++ src/components/listPage/UserList.js
@@ -1,6 +1,7 @@
-import { Box, Card } from "@material-ui/core";
+import { Box, Card, InputAdornment, TextField } from "@material-ui/core";
 import { styled } from "@material-ui/styles";
 
+import { AiOutlineSearch } from "react-icons/ai";
 import { BsFilter } from "react-icons/bs";
 import CustomIconButton from "../ui/IconButton";
 
@@ -8,7 +9,18 @@ const ContainerBoxStyle = styled(Box)(() => ({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-  border: "1px solid salmon",
+}));
+
+const TextFieldStyle = styled(TextField)(({ theme }) => ({
+  "& MuiInputBase-root": {
+    borderRadius: theme.spacing(1.25),
+  },
+  "& input": {
+    [theme.breakpoints.down("sm")]: {
+      padding: `${theme.spacing(1.25)} ${theme.spacing(0)}`,
+      maxWidth: theme.spacing(20),
+    },
+  },
 }));
 
 const UserList = () => {
@@ -19,13 +31,27 @@ const UserList = () => {
       sx={{
         py: 2.5,
         borderRadius: 1.5,
+        boxShadow: "0px 5px 15px -5px rgba(0,0,0,0.25)",
       }}>
       {/* Form & Button */}
       <ContainerBoxStyle
         sx={{
           px: 2.5,
         }}>
-        <h2>Form</h2>
+        <TextFieldStyle
+          type='search'
+          placeholder='Search user...'
+          aria-label='Search here'
+          id='outlined-start-adornment'
+          variant='outlined'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <AiOutlineSearch />
+              </InputAdornment>
+            ),
+          }}
+        />
 
         <CustomIconButton label='filter list'>
           <BsFilter />
